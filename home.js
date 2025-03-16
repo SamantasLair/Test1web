a86afe4ee4147e5a4e9fac5d2f69b3fa395d02bb
Samuel Ananta <94610303+SamantasLair@users.noreply.github.com>
2025-03-16 13:39:49 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,29 @@
+const modal = document.getElementById("loginModal");
+const btn = document.getElementById("loginBtn");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+function openTab(tabName) {
+    const tabContents = document.getElementsByClassName("tab-content");
+    for (let i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.remove("active");
+    }
+    
+    const tabButtons = document.getElementsByClassName("tab-button");
+    for (let i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].classList.remove("active");
+    }
+    
+    document.getElementById(tabName).classList.add("active");
+    event.currentTarget.classList.add("active");
+}
